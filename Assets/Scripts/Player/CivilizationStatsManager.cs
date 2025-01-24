16f77f2d7094d37a1461363668065fb1c67a24f4
PierceNunn <agentp5151@gmail.com>
2025-01-24 14:12:03 -0600

    set up OnTick (gradual population increase)

--- Assets/Scripts/Player/CivilizationStatsManager.cs
+++ Assets/Scripts/Player/CivilizationStatsManager.cs
@@ -6,7 +6,28 @@ public class CivilizationStatsManager : MonoBehaviour
 {
     [SerializeField] private int _population;
     [SerializeField] private float _resources;
+    [SerializeField] private float _tickTime;
 
     public int Population { get => _population; set => _population = value; }
     public float Resources { get => _resources; set => _resources = value; }
+
+    public void Start()
+    {
+        StartCoroutine(tickAdvance());
+    }
+
+    public void OnTick()
+    {
+        _population++;
+    }
+
+    public IEnumerator tickAdvance()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(_tickTime);
+            OnTick();
+        }
+        
+    }
 }
