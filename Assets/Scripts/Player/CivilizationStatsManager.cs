012c8c3c76fd4eec46d0df541afd0321bd504554
PierceNunn <agentp5151@gmail.com>
2025-01-24 13:59:26 -0600

    created CivilizationStatsManager.cs

---
+++ Assets/Scripts/Player/CivilizationStatsManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilizationStatsManager : MonoBehaviour
+{
+    [SerializeField] private int _population;
+    [SerializeField] private float _resources;
+}
