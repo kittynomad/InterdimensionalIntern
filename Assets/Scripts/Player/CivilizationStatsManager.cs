090165b32ffa0411d94361cdbb47823f2dde84eb
PierceNunn <agentp5151@gmail.com>
2025-01-24 14:33:27 -0600

    added happiness and happiness growth per tick

--- Assets/Scripts/Player/CivilizationStatsManager.cs
+++ Assets/Scripts/Player/CivilizationStatsManager.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CivilizationStatsManager : MonoBehaviour
 {
     [Header("Civilization Stats")]
-    [SerializeField] private int _population;
-    [SerializeField] private float _resources;
-    [SerializeField] private float _popGrowthPerTick;
+    [SerializeField] private int _population = 2;
+    [SerializeField] private float _resources = 10.4f;
+    [SerializeField] private float _popGrowthPerTick = 1f;
+    [SerializeField] private float _happinessGrowthPerTick = 1f;
+    [SerializeField] [Range(0.0f, 100.0f)] private float _happiness = 100f;
 
     [Header("Game Settings")]
-    [SerializeField] private float _tickTime;
+    [SerializeField] private float _tickTime = 1f;
     
     
 
@@ -27,6 +29,8 @@ public class CivilizationStatsManager : MonoBehaviour
         
         _population = Random.Range(0, 2) + 
             (int)((float)_population * _popGrowthPerTick);
+
+        _happiness *= _happinessGrowthPerTick;
     }
 
     public IEnumerator tickAdvance()
