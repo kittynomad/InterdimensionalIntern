ae4ef94df0a1f2579cc8befde3477f84726fecc6
PierceNunn <agentp5151@gmail.com>
2025-01-24 14:51:20 -0600

    added variables to BasicChoice

--- Assets/Scripts/ScriptableObjects/BasicChoice.cs
+++ Assets/Scripts/ScriptableObjects/BasicChoice.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 [CreateAssetMenu]
 public class BasicChoice : ScriptableObject
 {
+    [SerializeField] private string _choiceName;
+    [SerializeField] private string _choiceDescription;
 
+    [SerializeField] private StatModifier[] _statModifiers;
 }
