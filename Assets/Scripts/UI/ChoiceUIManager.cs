8fad509f600eff476b0ce2a3ee03d5702ee373d5
PierceNunn <agentp5151@gmail.com>
2025-02-14 15:06:06 -0600

    choice text now types out in real time

--- Assets/Scripts/UI/ChoiceUIManager.cs
+++ Assets/Scripts/UI/ChoiceUIManager.cs
@@ -9,22 +9,26 @@ public class ChoiceUIManager : MonoBehaviour
     [SerializeField] private TMP_InputField _inputField;
     [SerializeField] private ChoiceSet _choices;
     [SerializeField] private bool _retroMode = false;
+    [SerializeField] private float _textSpeed;
+
+    private bool isTyping;
 
     public void Start()
     {
-        foreach (TextMeshProUGUI t in _choiceTexts)
-            t.text = "";
-        for(int i = 0; i < _choices.Choices.Length; i++)
+        //foreach (TextMeshProUGUI t in _choiceTexts)
+            //t.text = "";
+        for (int i = 0; i < _choices.Choices.Length; i++)
         {
-            if(!_retroMode)
+            if (!_retroMode)
                 _choiceTexts[i].text = _choices.Choices[i].ToString();
             else
             {
                 _choiceTexts[0].text = _choiceTexts[0].text + "\n" + _choices.Choices[i].ToString();
                 _inputField.Select();
             }
-                
+
         }
+        StartCoroutine(TypeChoices(_choiceTexts[0].text));
     }
 
     public void ApplyChoice(int choiceToApply)
@@ -43,6 +47,26 @@ public class ChoiceUIManager : MonoBehaviour
         catch
         {
             Debug.LogError("attempt to convert string to int failed!");
+        }
+    }
+
+    IEnumerator TypeChoices(string sentence)
+    {
+        isTyping = true;
+
+        _choiceTexts[0].maxVisibleCharacters = 0;
+        _choiceTexts[0].text = sentence;
+        char[] sentenceCharArray = sentence.ToCharArray();
+
+        for (int i = 0; i < sentenceCharArray.Length; i++)
+        {
+            char letter = sentenceCharArray[i];
+
+            _choiceTexts[0].maxVisibleCharacters++;
+
+
+            //wait pre-specified time until printing the next letter
+            yield return new WaitForSeconds(_textSpeed);
         }
     }
 }
