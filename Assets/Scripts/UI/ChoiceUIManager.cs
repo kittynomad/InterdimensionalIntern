4a79bebf11db69ce0be0015264a22970fadb053d
PierceNunn <agentp5151@gmail.com>
2025-02-14 14:51:03 -0600

    choiceUI now prints all choice info on a single textmesh and auto selects input field

--- Assets/Scripts/UI/ChoiceUIManager.cs
+++ Assets/Scripts/UI/ChoiceUIManager.cs
@@ -6,13 +6,24 @@ using TMPro;
 public class ChoiceUIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI[] _choiceTexts;
+    [SerializeField] private TMP_InputField _inputField;
     [SerializeField] private ChoiceSet _choices;
+    [SerializeField] private bool _retroMode = false;
 
     public void Start()
     {
+        foreach (TextMeshProUGUI t in _choiceTexts)
+            t.text = "";
         for(int i = 0; i < _choices.Choices.Length; i++)
         {
-            _choiceTexts[i].text = _choices.Choices[i].ToString();
+            if(!_retroMode)
+                _choiceTexts[i].text = _choices.Choices[i].ToString();
+            else
+            {
+                _choiceTexts[0].text = _choiceTexts[0].text + "\n" + _choices.Choices[i].ToString();
+                _inputField.Select();
+            }
+                
         }
     }
 
