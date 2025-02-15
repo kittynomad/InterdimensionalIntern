e9345951dd0eda747cec0c7a40d56fc433ceed00
IceyFox006 <cgreenan@mail.bradley.edu>
2025-02-15 11:21:44 -0600

    Choice Interval/Pause Update
    Pauses Live Graph during choice screen
    If they player doesn't choose within the time limit, randomly chooses for them

--- Assets/Scripts/UI/ChoiceUIManager.cs
+++ Assets/Scripts/UI/ChoiceUIManager.cs
@@ -8,6 +8,8 @@ public class ChoiceUIManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI[] _choiceTexts;
     [SerializeField] private ChoiceSet _choices;
 
+    public ChoiceSet Choices { get => _choices; set => _choices = value; }
+
     public void Start()
     {
         for(int i = 0; i < _choices.Choices.Length; i++)
