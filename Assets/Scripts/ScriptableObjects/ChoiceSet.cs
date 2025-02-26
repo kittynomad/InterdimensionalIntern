78fca2a6d7928569f806fcf2eee12fb995a740f8
kittynomad <59345740+kittynomad@users.noreply.github.com>
2025-02-26 14:28:07 -0600

    added weight to ChoiceSet (set to 1 by default)

--- Assets/Scripts/ScriptableObjects/ChoiceSet.cs
+++ Assets/Scripts/ScriptableObjects/ChoiceSet.cs
@@ -8,6 +8,7 @@ public class ChoiceSet : ScriptableObject
     [SerializeField] private string _choiceSetName;
     [SerializeField] private Enums.CivilizationPhase _choicePhase;
     [SerializeField] private BasicChoice[] _choices; //set of choices available in this set
+    [SerializeField] [Range(0.0f, 100.0f)] private float _weight = 1f;
 
     public string ChoiceSetName { get => _choiceSetName; set => _choiceSetName = value; }
     public Enums.CivilizationPhase ChoicePhase { get => _choicePhase; set => _choicePhase = value; }
