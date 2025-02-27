e74fb23d0bbaf9c9bf3e7c2db8a8b0c8bf3302df
kittynomad <59345740+kittynomad@users.noreply.github.com>
2025-02-26 18:25:44 -0600

    fixed StatScalingChoiceSet not appearing in asset menu

--- Assets/Scripts/ScriptableObjects/StatScalingChoiceSet.cs
+++ Assets/Scripts/ScriptableObjects/StatScalingChoiceSet.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class StatScalingChoiceSet : ChoiceSet
 {
     [SerializeField] private StatWithAssociatedValue[] _minimumValuestoScale;
